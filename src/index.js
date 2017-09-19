cb8c98209605e215ce6a868286ffd111755ab804
Ed Ropple <ed@edropple.com>
2017-09-19 03:47:54 -0400

    added timers plugin

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import * as TowerCGDashboard from '@towercg/dashboard';
 import './index.css';
 
 import ExamplePlugin from '@towercg-plugins/example-dashboard';
+import * as TwitchDashboard from '@towercg-plugins/twitch-dashboard';
+import * as TimersDashboard from '@towercg-plugins/timers-dashboard';
 
 const config = new TowerCGDashboard.Config();
 
 config.registerPlugin(ExamplePlugin);
+config.registerPlugin(TwitchDashboard.Plugins.GameInfo, { channel: "#tracecomplete_test" });
+config.registerPlugin(TimersDashboard.Plugins.Manager);
 
 TowerCGDashboard.startApp(config);
